f0f3bf671f7d919a1f71ba3ef5476333e4418e50
kanthikakypss <kanthikakyps@gmail.com>
2023-06-12 13:26:39 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import './App.css'
 const App = () => {
   const [users, setUsers] = useState([])
 
+  const [allOrder, setAllOrder] = useState(0);
+  const [successOrder, setSuccessOrder] = useState(0);
+  const [UnSuccessOrder, setUnSuccessOrder] = useState(0);
+
   const fetchUserData = async () => {
     try {
       const res = await axios.get(
@@ -22,10 +26,40 @@ const App = () => {
     fetchUserData()
   }, [])
 
+  useEffect(() => {
+    // Fetch menu items from the database
+    setAllOrder(users.length);
+    setSuccessOrder(users.filter((obj) => obj.cookstatus === 0).length);
+    setUnSuccessOrder(users.filter((obj) => obj.cookstatus === 1).length);
+  }, [users]);
+
+
+  console.log(users);
+
   return (
     <div>
       <div className="grid gap-4 p-8 ">
         <div id="title-header" className="text-3xl text-center">For Cashier</div>
+        <div id="tab-filter" className="flex justify-center gap-2">
+          <div className="flex items-center bg-blue-200 py-2 px-4 rounded-full ">
+            ออเดอร์ทั้งหมด
+            <div className="flex justify-center items-center ml-2 rounded-full bg-white w-8 h-8">
+              {allOrder}
+            </div>
+          </div>
+          <div className="flex items-center bg-red-200 py-2 px-4 rounded-full">
+            ยังไม่ได้ทำ
+            <div className="flex justify-center items-center ml-2 rounded-full bg-white w-8 h-8">
+              {successOrder}
+            </div>
+          </div>
+          <div className="flex items-center bg-green-200 py-2 px-4 rounded-full">
+            ทำแล้ว
+            <div className="flex justify-center items-center ml-2 rounded-full bg-white w-8 h-8">
+              {UnSuccessOrder}
+            </div>
+          </div>
+        </div>
         <div id="tab-filter" className="flex justify-center gap-2">
           <div className="w-full">
             <div className="grid gap-4 p-2"></div>
@@ -36,7 +70,16 @@ const App = () => {
                     <div className="flex gap-8 items-center">
                       <div>{user.numoftable}</div>
                       <div>วันที่และเวลา : {user.ordertime}</div>
-                    </div>
+                      </div>
+                    {/* check status */}
+                    {user.cookstatus === 1 ? (
+                      <div className="bg-green-200 py-2 px-4 rounded-full">ทำแล้ว</div>
+                    ) : (
+                      <div className="bg-red-200 py-2 px-4 rounded-full">
+                        ยังไม่ได้ทำ
+                      </div>
+                    )}
+                
                   </div>
                   <div className="flex justify-center bg-white">
                     <table className="w-full">
@@ -60,8 +103,8 @@ const App = () => {
                       </tbody>
                     </table>
                   </div>
-                  <div className="font-bold py-2 px-4 rounded-b-xl w-full text-center border-y  bg-blue-300">ราคาทั้งหมด  {user.totalprice} บาท</div>
-                  
+                  <div className="font-bold py-2 px-4 rounded-b-xl w-full text-center border-y">ราคาทั้งหมด  {user.totalprice} บาท</div>
+                 
                 </div>
               </div>
             ))}
